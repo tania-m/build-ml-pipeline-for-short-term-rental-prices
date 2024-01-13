93bf48683069ae419811bf34f923729ab474bf0e
Tania M <tania.mathern@gmail.com>
2024-01-12 21:00:16 -0800

    Update EDA with profiling report

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ mlflow run . -P steps=download
 # Manually run basic cleaning in MLflow with full code source path
 mlflow run src/basic_cleaning -P parameter1=1 -P parameter2=2 -P parameter3="test"
 
-# Run EDA 
-# Alternative for Jupyter notebook without MLflow: jupyter-notebook --no-browser
+# Run EDA: Pipeline mode
 mlflow run src/eda
+# RUn EDA: Alternative for Jupyter notebook without MLflow: eg. when running on WSL
+jupyter-notebook --no-browser
