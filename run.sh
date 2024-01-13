8914578bae39ee2d57c1efa17b05b7ad5bd911bc
Tania M <tania.mathern@gmail.com>
2024-01-12 19:34:53 -0800

    Fix comment for commands

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 mlflow run . -P steps=download
 
 # Manually run basic cleaning in MLflow with full code source path
-# Alternative for Jupyter notebook without MLflow: jupyter-notebook --no-browser
 mlflow run src/basic_cleaning -P parameter1=1 -P parameter2=2 -P parameter3="test"
 
 # Run EDA 
+# Alternative for Jupyter notebook without MLflow: jupyter-notebook --no-browser
 mlflow run src/eda
