0df95a062314ecb7685e5d388abf99c3ce94c1c8
Tania M <tania.mathern@gmail.com>
2024-01-12 19:32:42 -0800

    Add more initial commands examples

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
-# Manually run basic cleaning in MLflow
+#!/usr/bin/env bash
+
+# Download dataset (step defined in root), stores in W&B as sample.csv
+mlflow run . -P steps=download
+
+# Manually run basic cleaning in MLflow with full code source path
+# Alternative for Jupyter notebook without MLflow: jupyter-notebook --no-browser
 mlflow run src/basic_cleaning -P parameter1=1 -P parameter2=2 -P parameter3="test"
+
+# Run EDA 
+mlflow run src/eda
